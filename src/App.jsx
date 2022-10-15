0d3995ea2aabcff449bf775a641a37b86b82d47e
AlyssaD <pink_lime@live.com>
2022-10-14 21:43:33 -0400

    add landing and burgerShop routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './styles/App.css'
+import { Route, Routes } from 'react-router-dom'
 
 // Components
 import Landing from './Components/Landing/Landing'
+import BurgerShop from './Components/BurgerShop/BurgerShop'
 
 const App = () => {
   return (
     <main>
+      <Routes>
+        <Route to='/burgers' element={<BurgerShop />} />
+      </Routes>
       <Landing />
     </main>
   )
