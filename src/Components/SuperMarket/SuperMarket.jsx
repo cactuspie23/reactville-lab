4e330071a6f2b533faea1cbd1db36c4cf0a497fe
AlyssaD <pink_lime@live.com>
2022-10-19 17:46:48 -0400

    add to cart functionality

--- src/Components/SuperMarket/SuperMarket.jsx
+++ src/Components/SuperMarket/SuperMarket.jsx
@@ -11,11 +11,27 @@ const SuperMarket = () => {
   const [cart, setCart] = useState([])
   const [productCategory, setProductCategory] = useState('Produce')
 
+  const addToCart = item => {
+    const isItemInCart = cart.some(prod => prod.id === item.id)
+    if (isItemInCart) {
+      setCart(cart.map(prod => prod.id === item.id
+        ? {...prod, quantity: prod.quantity + 1}  
+        : prod
+      ))
+    } else {
+      setCart([{...item, quantity: 1}, ...cart])
+    }
+  }
+
   return (
     <div className="super-market">
       <section>
         <MarketNav products={products} setProductCategory={setProductCategory} />
-        <DisplayProducts products={products} productCategory={productCategory} />
+        <DisplayProducts 
+          products={products} 
+          productCategory={productCategory} 
+          addToCart={addToCart}
+        />
       </section>
 
       <Cart cart={cart} />
